e9feefd348d72fc163875117348ad362b19e06bb
Lorso <putyushkin2@yandex.ru>
2022-10-18 22:12:44 +0300

    dz6

---
+++ src/main/java/dz6/Main.java
@@ -0,0 +1,19 @@
+package dz6;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog sokar = new Dog("Сокар");
+        Cat tima = new Cat("Тима");
+
+
+        sokar.actionRun(15);
+        sokar.actionSwim(15);
+
+        tima.actionRun(600);
+        tima.actionSwim(1);
+
+
+
+
+    }
+}
